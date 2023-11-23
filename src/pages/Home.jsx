de5e2963baab90b66176339f625c11cbb49b9e27
gorkemsandikci <gorkemsandikci@gmail.com>
2023-11-23 18:04:02 +0300

    island added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,23 @@ import Island from '../models/Island.jsx';
             </div>*/
 }
 const Home = () => {
+    const adjustIslandForScreenSize = () => {
+        let screenScale = null;
+        let screenPosition= [0, -6.5, -43];
+        let rotation = [0.1, 4.7, 0];
+
+        if(window.innerWidth <768) {
+            screenScale = [0.9, 0.9, 0.9];
+            screenPosition = [0, -6.5, -43];
+        } else {
+            screenScale = [1, 1, 1];
+        }
+
+        return [screenScale, screenPosition];
+    }
+
+    const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
     return (
         <section className="w-full h-screen relative">
             <Canvas
@@ -16,12 +33,15 @@ const Home = () => {
                 camera={{near: 0.1, far: 1000}}
             >
                 <Suspense fallback={<Loader />}>
-                    <directionalLight />
-                    <ambientLight />
-                    <pointLight />
-                    <spotLight />
-                    <hemisphereLight />
-                    <Island />
+                    <directionalLight position={[1, 1, 1]} intensity={2}/>
+                    <ambientLight intensity={0.5}/>
+                    <hemisphereLight skyColor="#b1e1ff" groundColor="#000000"  intensity={1}/>
+
+                    <Island
+                        position = {islandPosition}
+                        scale = {islandScale}
+                        rotation = {islandRotation}
+                    />
                 </Suspense>
 
             </Canvas>
