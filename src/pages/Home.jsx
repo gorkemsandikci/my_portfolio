f17e59346f62ffff0e5410bdecd07a764d9112b9
gorkemsandikci <gorkemsandikci@gmail.com>
2023-11-23 23:11:03 +0300

    sky added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import {Canvas} from '@react-three/fiber';
 import Loader from "../components/Loader.jsx";
 
 import Island from '../models/Island.jsx';
+import Sky from "../models/Sky.jsx";
 
 {/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
                 POPUP
@@ -37,6 +38,9 @@ const Home = () => {
                     <ambientLight intensity={0.5}/>
                     <hemisphereLight skyColor="#b1e1ff" groundColor="#000000"  intensity={1}/>
 
+                    <Sky
+
+                    />
                     <Island
                         position = {islandPosition}
                         scale = {islandScale}
