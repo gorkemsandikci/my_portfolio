27c435ecfc68b3a3cb4dc762317d6cec902e6894
gorkemsandikci <gorkemsandikci@gmail.com>
2023-12-14 16:38:58 +0300

    Home page info popup added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,8 @@ import Island from '../models/Island.jsx';
 import Sky from "../models/Sky.jsx";
 import Bird from "../models/Bird.jsx";
 import Plane from "../models/Plane.jsx";
+import HomeInfo from "../components/HomeInfo.jsx";
 
-{/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-                POPUP
-            </div>*/
-}
 const Home = () => {
     const [isRotating, setIsRotating] = useState(false)
     const [currentStage, setCurrentStage] = useState(1);
@@ -47,6 +44,9 @@ const Home = () => {
 
     return (
         <section className="w-full h-screen relative">
+            <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                {currentStage && <HomeInfo currentStage={currentStage} />}
+            </div>
             <Canvas
                 className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
                 camera={{near: 0.1, far: 1000}}
